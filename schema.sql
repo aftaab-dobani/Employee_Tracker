da7950b4bdbe05eac9baeb5aeb5c6154d1d8a84d
Aftaab Dobani <1234dobani@gmail.com>
2021-05-07 11:02:06 -0400

    added not null

--- schema.sql
+++ schema.sql
@@ -4,24 +4,30 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id INTEGER(11) NOT NULL,
-    department_name VARCHAR(30),
+    id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INTEGER(11),
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER(30)
+    id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER(30) INT NOT NULL,
+    PRIMARY KEY (id)
 ); 
 
 CREATE TABLE employee(
-    id INTEGER(11),
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER(11),
-    manager_id INTEGER(30) 
+    id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER(11) INT NOT NULL,
+    manager_id INTEGER(30) INT NOT NULL,
     PRIMARY KEY(id)
 );
+SELECT * FROM department;
+
+SELECT * FROM role;
+
+SELECT * FROM employee; 
 
---
+++ seeds.sql
@@ -0,0 +1,12 @@
+USE employees_db;
+
+INSERT INTO department (name)
+VALUES (1, "BEDDING"), (2, "Sneakers"), (3, "MENS"), (4, "WOMENS"); 
+
+INSERT INTO role (name)
+VALUES (1, "MANAGER", 100000, 3),
+(2, "SALES MANAER", 90000, 2), (
+    3, "SALES REP", 60000, 
+); 
+
+INSERT INTO employee
